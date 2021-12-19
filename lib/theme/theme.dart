ab5e193f07ce8d31e1b3631f2451cffc59da8c2c
Mahdinaghikhani <mahdinkh7@gmail.com>
2021-12-19 16:51:01 +0330

    create-getstart

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,5 +3,11 @@ import 'package:flutter/material.dart';
 class MyThemes {
   static final darkTheme = ThemeData(scaffoldBackgroundColor: Colors.black);
   static final lightTheme = ThemeData(
-      scaffoldBackgroundColor: Colors.white, primaryTextTheme: TextTheme());
+      scaffoldBackgroundColor: Colors.white,
+      primaryTextTheme: TextTheme(
+          button: TextStyle(color: Color(0xFFDC9298)),
+          subtitle1: TextStyle(
+              color: Color(0xFFDC9298),
+              fontWeight: FontWeight.bold,
+              fontSize: 18)));
 }
--- lib/views/auth/getstart_screans.dart
+++ lib/views/auth/getstart_screans.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pantomim/views/auth/home_screans.dart';
 
 class GetStartScreans extends StatelessWidget {
   const GetStartScreans({Key? key}) : super(key: key);
@@ -6,21 +7,53 @@ class GetStartScreans extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(25),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+              width: double.infinity,
+              height: 70,
+              color: Color(0xFFDC9298),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Color(0xFFDC9298)),
+                child: Text('data'),
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => HomeScreans()));
+                },
+              )),
+        ),
+      ),
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SafeArea(
-              child: Padding(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Padding(
                 padding: EdgeInsets.all(10),
                 child: Image.asset(
                   'assets/images/getstart.png',
                   width: 400,
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  '😍 خوش اومدید \n شما می تونید بازی پانتومیم یا ادا بازی کنید \nکلی لذت ببرید ',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).primaryTextTheme.subtitle1,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

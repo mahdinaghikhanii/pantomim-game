0344e5bb149260e00940bbcd5c397132a5f0c0cf
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-02-24 20:54:07 +0330

    add text and imagae and change background color in finishid screans

--- lib/views/dialog_screns/dialog_test.dart
+++ lib/views/dialog_screns/dialog_test.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pantomim/generated/l10n.dart';
+import 'package:pantomim/theme/constant.dart';
+import 'package:pantomim/util/view_models/provider/provider.dart';
+import 'package:pantomim/views/widgets/double_floatingac_button.dart';
+import 'package:pantomim/views/widgets/scoreshowtext_textdetail.dart';
+import 'package:provider/provider.dart';
 
 class DialogFinishidGame extends StatelessWidget {
   const DialogFinishidGame({Key? key}) : super(key: key);
@@ -6,7 +12,8 @@ class DialogFinishidGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
+    var localApp = S.of(context);
+    final modelProvier = Provider.of<AppProvider>(context);
     return Dialog(
         insetPadding:
             const EdgeInsets.only(left: 25, right: 25, top: 120, bottom: 150),
@@ -25,12 +32,77 @@ class DialogFinishidGame extends StatelessWidget {
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(30),
                         color: Colors.transparent),
-                    child: Image.asset(
-                      'assets/images/medal.png',
-                      width: 200,
-                      height: 200,
+                    child: Image.asset('assets/images/medal.png',
+                        width: size.width * 0.55, height: size.height * 0.17),
+                  )),
+              Container(
+                padding: const EdgeInsets.all(20),
+                width: size.width * 0.83,
+                height: size.height * 0.52,
+                decoration: BoxDecoration(
+                    color: Colors.white24,
+                    borderRadius: BorderRadius.circular(50)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 45,
                     ),
-                  ))
+                    Center(
+                        child: Text(
+                      "Game Is Finishid",
+                      style: Theme.of(context).textTheme.headline5,
+                    )),
+                    WidgetShowTextWinOrLoseDetail(
+                      title: 'Word rating :',
+                      score: modelProvier.getCounterOfScoreTeam1().toString(),
+                    ),
+                    const WidgetShowTextWinOrLoseDetail(
+                      title: 'Respond quickly :',
+                      score: '2',
+                    ),
+                    const WidgetShowTextWinOrLoseDetail(
+                      title: 'Error in the game  :',
+                      score: '0',
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40, bottom: 10),
+                      child: Center(
+                        child: DounleFloattingButton(
+                          colorBtn: modelProvier.checkbtnwinlose ? kblue : kred,
+                          ontap: () {
+                            if (modelProvier.getcheckforInnigns >
+                                modelProvier.getCounterTeam()) {
+                              showDialog(
+                                  context: context,
+                                  builder: (
+                                    context,
+                                  ) =>
+                                      const DialogFinishidGame());
+                            } else {
+                              if (modelProvier.checkbtnwinlose == true) {
+                                modelProvier.setaddcheckforInnigns();
+                              } else {}
+
+                              if (modelProvier.getcheckforInnigns >
+                                  modelProvier.getCounterTeam()) {
+                                showDialog(
+                                    context: context,
+                                    builder: (
+                                      context,
+                                    ) =>
+                                        const DialogFinishidGame());
+                              }
+                            }
+                          },
+                          textBtn: localApp.scoreScreansBtnText,
+                          highlightColor: kblue,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
             ]));
   }
 }
--- lib/views/widgets/scoreshowtext_textdetail.dart
+++ lib/views/widgets/scoreshowtext_textdetail.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 class WidgetShowTextWinOrLoseDetail extends StatelessWidget {
   final String title;
   final String score;
+
   // ignore: use_key_in_widget_constructors
-  const WidgetShowTextWinOrLoseDetail(
-      {required this.score, required this.title});
+  const WidgetShowTextWinOrLoseDetail({
+    required this.score,
+    required this.title,
+  });
 
   @override
   Widget build(BuildContext context) {

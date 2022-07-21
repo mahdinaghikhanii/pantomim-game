9d45fb484dcc3da87473e7612db0ad8a56321d0a
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-07-21 11:13:24 +0430

    add hive_generetor and create model team

---
+++ lib/models/team.dart
@@ -0,0 +1,13 @@
+class TeamModel {
+  String team1;
+  String team2;
+  String team3;
+  String team4;
+
+  int numberOfTeams;
+  int numberofRoundsGame;
+  int numberofRoundsGameTitle;
+
+  TeamModel(this.team1, this.team2, this.team3, this.team4, this.numberOfTeams,
+      this.numberofRoundsGame, this.numberofRoundsGameTitle);
+}
